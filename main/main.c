88cac400e388061f3c66c2d048ff6b3af445a653
Chris Atkin <atkinchris@gmail.com>
2018-06-07 10:07:27 +0100

    Add u8g2 sample code

--- main/main.c
+++ main/main.c
@@ -1,9 +1,17 @@
-#include <stdio.h>
-#include <freertos/FreeRTOS.h>
+#include "freertos/FreeRTOS.h"
 #include <freertos/task.h>
-#include <esp_system.h>
-#include <u8x8.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include <driver/spi_master.h>
+#include <esp_log.h>
 
-void app_main() {
-    printf("hello world\n");
+#include "test_SSD1306_i2c.c"
+
+void app_main(void) {
+	nvs_flash_init();
+	xTaskCreatePinnedToCore(&task_test_SSD1306i2c, "task_test_SSD1306i2c", 8048, NULL, 5, NULL, 0);
 }
